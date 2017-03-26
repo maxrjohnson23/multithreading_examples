7bd59b2b9a737ca381ed9ab888971291c0dcbb6f
Max <maxrjohnson23@gmail.com>
2017-03-26 15:01:03 -0500

    Fixed double check locking bug, but returned to same performance issues

--- src/com/max/singleton/Singleton.java
+++ src/com/max/singleton/Singleton.java
@@ -5,12 +5,14 @@ package com.max.singleton;
  */
 public class Singleton {
 
-    private static Singleton instance;
+    // Make volative to create happens before link
+    private static volatile Singleton instance;
     private static final Object key = new Object();
 
     private Singleton() {}
 
     // Attempt to fix for multicore CPUs to prevent synchronizing the read operation
+    // Fix is error prone because a read can happen on a partially built instance
     public static Singleton getInstance() {
         if (instance == null) {
             instance = new Singleton();
