1cb7fd4c894de37e546cfc2ac3b6f1c6c13b1824
Max <maxrjohnson23@gmail.com>
2017-03-26 15:25:04 -0500

    Add Worker example to demonstrate locking using an object

---
+++ src/com/max/worker/Worker.java
@@ -0,0 +1,66 @@
+package com.max.worker;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Max on 3/26/2017.
+ */
+public class Worker extends Thread {
+
+    private final Object lock = new Object();
+    private volatile boolean quittingTime = false;
+
+    public void run() {
+        while (!quittingTime) {
+            working();
+            System.out.println("Still working...");
+        }
+
+        System.out.println("Coffee is good!");
+    }
+
+    private void working() {
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            // do nothing
+        }
+    }
+
+    synchronized void quit() throws InterruptedException {
+
+        synchronized (lock) {
+            quittingTime = true;
+            System.out.println("Calling join");
+            join();
+            System.out.println("Back from join");
+        }
+    }
+
+    synchronized void keepWorking() {
+        synchronized (lock) {
+            quittingTime = false;
+            System.out.println("Keep working");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final Worker worker = new Worker();
+        worker.start();
+
+        Timer t = new Timer(true); // daemon thread
+        t.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                worker.keepWorking();
+            }
+        }, 500);
+
+        Thread.sleep(400);
+        worker.quit();
+    }
+
+}
